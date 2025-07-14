3d2c973a0e23ae33a0a6d671833c709916c0a63a
Romdoni <romdoni.agung@sicepat.com>
2025-07-14 14:49:17 +0700

    change to module

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "hello_world" {
-  ami           = data.aws_ami.ubuntu.id
-  subnet_id     = data.aws_subnets.default.ids[0]
-  instance_type = "t3.micro"
+    ami           = data.aws_ami.ubuntu.id
+#   subnet_id     = data.aws_subnets.default.ids[0]
+    subnet_id     = var.subnet_id
+    instance_type = var.instance_type
 
-  tags = {
-    Name = "hello_world" // Sets the display name in AWS
-  }
+    tags = {
+        Name = "hello_world" // Sets the display name in AWS
+    }
 }
--- providers.tf
+++ providers.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 
-provider "aws" {
-    region  = var.aws_region
-    profile = var.aws_profile
-}
+# provider "aws" {
+#   region  = var.aws_region
+#   profile = var.aws_profile
+# }
