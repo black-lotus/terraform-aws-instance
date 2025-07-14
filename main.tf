0388ba475a492fa29ab6a8b4308e2a5c6fceef84
Romdoni <romdoni.agung@sicepat.com>
2025-07-14 09:44:35 +0700

    Initial commit

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "hello_world" {
+  ami           = data.aws_ami.ubuntu.id
+  subnet_id     = data.aws_subnets.default.ids[0]
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "hello_world" // Sets the display name in AWS
+  }
+}
