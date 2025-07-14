93e63531fadba9d5774a1ab846a358317d483eeb
Romdoni <romdoni.agung@sicepat.com>
2025-07-14 14:42:16 +0700

    change to module

---
+++ example/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {                                                  
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+ 
+variable "region" {                                          
+  type        = string
+  default     = "ap-southeast-1"
+  description = "The AWS Region to connect and run the tests in."
+}
+
+variable "aws_profile" {
+  description = "The AWS profile to use for authentication."
+  type        = string
+  default     = "sicepat-experiment"
+}
+ 
+provider "aws" {                                             
+  region = var.region   
+  profile = var.aws_profile                                     
+}
