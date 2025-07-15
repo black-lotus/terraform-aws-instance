7d6e0f9e43269a0ad50695bae39e9c4d4367a81c
Romdoni <romdoni.agung@sicepat.com>
2025-07-15 09:41:40 +0700

    Implement count and tags

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
 resource "aws_instance" "hello_world" {
-    ami           = data.aws_ami.ubuntu.id
-#   subnet_id     = data.aws_subnets.default.ids[0]
+    count         = var.instance_count
+    ami         = data.aws_ami.ubuntu.id
+    #   subnet_id     = data.aws_subnets.default.ids[0]
     subnet_id     = var.subnet_id
     instance_type = var.instance_type
 
-    tags = {
-        Name = "hello_world" // Sets the display name in AWS
-    }
+    # tags = {
+    #     Name = "hello_world" // Sets the display name in AWS
+    # }
+
+    tags = merge(var.tags, {
+        Name = "${var.name_prefix}-${count.index}"
+    })
 }
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "aws_instance_arn" {
   description = "The AWS Resource Name for the instance."
-  value       = aws_instance.hello_world.arn
+  value       = aws_instance.hello_world[*].arn
 }
 
 output "aws_instance_ip" {
   description = "The IP Address for the private network interface on the instance."
-  value       = aws_instance.hello_world.private_ip
+  value       = aws_instance.hello_world[*].private_ip
 }
 
 output "aws_instance" {
